69eec96095459c365df5ea6581952c2f554e08b6
Adnaan Badr <badr.adnaan@gmail.com>
2021-11-12 16:27:09 +0530

    add kubernetes/mod.ts

---
+++ kubernetes/mod.ts
@@ -0,0 +1,11 @@
+export { iterateContainers } from "./src/resources.js";
+export { ImageReference } from "./src/image-reference.ts";
+export { valuesForGenerate } from "./src/generate.js";
+export { generateFromShorts } from "./src/short/generate.js";
+export {
+  generateKustomization,
+  kustomization,
+  overlay,
+} from "./src/overlay/index.js";
+
+export { chart, generateChart } from "./src/chart/index.js";
