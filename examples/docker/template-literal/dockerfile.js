de9703eef527f4a7a02ff58418ca5e26b15589f7
Adnaan Badr <badr.adnaan@gmail.com>
2021-11-13 20:05:09 +0530

    update examples

--- examples/docker/template-literal/dockerfile.js
+++ examples/docker/template-literal/dockerfile.js
@@ -1,4 +1,4 @@
-import { write } from 'https://deno.land/x/dxcfg/mod.ts'
+import { write } from 'https://deno.land/x/dxcfg@v0.0.3/mod.ts'
 import { Dockerfile } from './docker.js';
 
 const myService = {
