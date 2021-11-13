af5c03cd6add3c26ed27baebaa73484fd2f4f3b6
Adnaan Badr <badr.adnaan@gmail.com>
2021-11-13 20:07:48 +0530

    updated mod.ts

--- mod.ts
+++ mod.ts
@@ -1,8 +1,7 @@
-export { Format, write } from "./write.ts";
-export { Encoding, read } from "./read.ts";
+export { Format, write, writeSync } from "./write.ts";
+export { Encoding, read, readSync } from "./read.ts";
 export { deep, deepWithKey, first, merge, replace } from "./merge.ts";
 export { validateWithFile, validateWithObject } from "./schema.ts";
-import * as param from "./param.ts";
-export { param };
+export { param, paramSync } from "./param.ts";
 export { parse, stringify } from "./parse.ts";
 export { dir, info } from "./fs.ts";
