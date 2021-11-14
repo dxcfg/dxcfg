cd51784e4d8f4c62dbb8e20d5ca7179cc114eaf3
Adnaan Badr <badr.adnaan@gmail.com>
2021-11-14 13:28:11 +0530

    updated examples

--- examples/deps.ts
+++ examples/deps.ts
@@ -1,8 +1,12 @@
-export * from "https://deno.land/x/dxcfg@v0.0.6/mod.ts";
-export * from "https://deno.land/x/dxcfg@v0.0.6/kubernetes/mod.ts";
-export * from "https://deno.land/x/dxcfg@v0.0.6/kubernetes/gen/mod.ts";
+export * from "https://deno.land/x/dxcfg@v0.0.7/mod.ts";
+export * from "https://deno.land/x/dxcfg@v0.0.7/kubernetes/mod.ts";
+export * from "https://deno.land/x/dxcfg@v0.0.7/kubernetes/gen/mod.ts";
 export {
   assertEquals,
   assertObjectMatch,
   assertThrows,
 } from "https://deno.land/std@0.114.0/testing/asserts.ts";
+
+import HandlebarsJS from "https://dev.jspm.io/handlebars@4.7.6";
+export { HandlebarsJS };
+export { dir } from "../fs.ts";
--- examples/kubernetes/chart-simple/test.param.cmd
+++
@@ -1,2 +0,0 @@
-npm ci
-jk generate --stdout ./index.js -p values.name=demo -p values.image.tag=v2
