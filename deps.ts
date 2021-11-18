b3eb25984501bb8584c94235d650cfa2c8fc4f54
Adnaan Badr <badr.adnaan@gmail.com>
2021-11-18 12:33:08 +0530

    remove readjsonlines package

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
+// std
 export {
   parse as yamlParse,
   parseAll as yamlParseAll,
@@ -12,25 +13,23 @@ export {
   assertObjectMatch,
   assertThrows,
 } from "https://deno.land/std/testing/asserts.ts";
+export { ensureFile, ensureFileSync } from "https://deno.land/std/fs/mod.ts";
+export { encode as base64encode } from "https://deno.land/std/encoding/base64.ts";
+export { parse as flagsParse } from "https://deno.land/std/flags/mod.ts";
+import * as path from "https://deno.land/std/path/mod.ts";
+export { path };
 
-import Ajv, { ValidateFunction } from "https://esm.sh/ajv@8.6.3";
+// third-party
+import Ajv, { ValidateFunction } from "https://esm.sh/ajv@8.8.1";
 export type { ValidateFunction };
 import addFormats from "https://esm.sh/ajv-formats@2.1.1";
 const ajv = new Ajv({ allErrors: true });
 addFormats(ajv);
 export { ajv };
 
-export { parse as flagsParse } from "https://deno.land/std/flags/mod.ts";
-import * as path from "https://deno.land/std/path/mod.ts";
-export { path };
-
 import set from "https://deno.land/x/lodash@4.17.15-es/set.js";
 export { set as setValue };
 import get from "https://deno.land/x/lodash@4.17.15-es/get.js";
 export { get as getValue };
-export { encode as base64encode } from "https://deno.land/std/encoding/base64.ts";
 import HandlebarsJS from "https://dev.jspm.io/handlebars@4.7.6";
 export { HandlebarsJS };
-import readJsonLines from "https://cdn.skypack.dev/read-json-lines-sync?dts";
-export { readJsonLines };
-export { ensureFile, ensureFileSync } from "https://deno.land/std/fs/mod.ts";
